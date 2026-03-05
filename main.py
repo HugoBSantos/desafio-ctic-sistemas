9e34fb7f4c5e25487a4d360c7804bf4ccd2ea6d5
HugoSantos14 <hugosantos14000@gmail.com>
2026-03-05 14:59:55 -0300

    feat: add student list by registration status on disciplines

--- main.py
+++ main.py
@@ -5,8 +5,8 @@ from pathlib import Path
 
 FILE_PATH = Path().cwd() / "data" / "BASE_TESTE_HUGO1.csv"
 
-def matriculados_por_disciplina(df: pd.DataFrame) -> pd.DataFrame:
-    df_matriculados = df[df["SIT_DISCIPLINA"] == "CURSANDO"]
+def alunos_por_disciplina(df: pd.DataFrame, sit_disciplina: str) -> pd.DataFrame:
+    df_alunos = df[df["SIT_DISCIPLINA"] == sit_disciplina.upper()]
     cols = ["CODIGO_DISCIPLINA", "DISCIPLINA", "NOME_ABREV_ALUNO"]
     return df_matriculados[cols].sort_values(by="DISCIPLINA")
 
@@ -14,6 +14,39 @@ if __name__ == "__main__":
     st.header("📊 Dados dos alunos da EC5MA")
     df_alunos = pd.read_csv(Path(FILE_PATH), encoding="latin1", sep="\t")
     
-    st.markdown("### Alunos matriculados por disciplina")
+    disciplinas = {
+        codigo: disciplina
+        for codigo, disciplina in zip(
+            df_alunos["CODIGO_DISCIPLINA"], df_alunos["DISCIPLINA"]
+        )
+    }
     
-    st.write(matriculados_por_disciplina(df_alunos))
+    filtro_codigo = st.sidebar.multiselect(
+        "Filtrar por código da disciplina",
+        disciplinas.keys()
+    )
+    
+    filtro_disciplina = st.sidebar.multiselect(
+        "Filtrar por nome da disciplina",
+        disciplinas.values()
+    )
+    
+    st.markdown("### Lista de alunos por disciplina")
+    matriculados, em_ajuste = st.tabs(["Matriculados", "Em ajuste"])
+    
+    with matriculados:
+        st.markdown("#### Alunos matriculados por disciplina")
+
+        df_filtrado = alunos_por_disciplina(df_alunos, sit_disciplina="cursando")
+        df_filtrado = filtrar_df(df_filtrado, "CODIGO_DISCIPLINA", filtro_codigo)
+        df_filtrado = filtrar_df(df_filtrado, "DISCIPLINA", filtro_disciplina)
+        
+        st.dataframe(df_filtrado)
+    with em_ajuste:
+        st.markdown("#### Alunos em situação de ajuste por disciplina")
+
+        df_filtrado = alunos_por_disciplina(df_alunos, sit_disciplina="ajuste")
+        df_filtrado = filtrar_df(df_filtrado, "CODIGO_DISCIPLINA", filtro_codigo)
+        df_filtrado = filtrar_df(df_filtrado, "DISCIPLINA", filtro_disciplina)
+        
+        st.dataframe(df_filtrado)
